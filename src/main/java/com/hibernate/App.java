f0d3631d007383895eeefc43977e95edb645d26a
Shivang Yadav <01yadavshivang@gmail.com>
2025-03-03 23:06:34 +0530

    Initial commit

---
+++ src/main/java/com/hibernate/App.java
@@ -0,0 +1,25 @@
+package com.hibernate;
+
+import org.hibernate.cfg.Configuration;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
+import com.hibernate.entity.Employee;
+import org.hibernate.Session;
+
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Employee e =new Employee(2,"Ankur","male",94009);
+
+Configuration cfg =new Configuration().configure("hibernate.cfg.xml");
+SessionFactory sessionFactory=cfg.buildSessionFactory();
+Session session=sessionFactory.openSession();	
+Transaction tx=session.beginTransaction();
+	session.save(e);
+	tx.commit();
+	}
+
+}
