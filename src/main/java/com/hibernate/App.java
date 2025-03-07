973fd4e2a6211dc6e8330957e920530bdcdbb650
Shivang Yadav <01yadavshivang@gmail.com>
2025-03-07 22:51:08 +0530

    update

--- src/main/java/com/hibernate/App.java
+++ src/main/java/com/hibernate/App.java
@@ -1,13 +1,15 @@
 package com.hibernate;
 
 import org.hibernate.cfg.Configuration;
-
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.boot.Metadata;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.boot.registry.StandardServiceRegistry;
+
+import com.hibernate.config.HibernateConfiguration;
 import com.hibernate.entity.Employee;
 import org.hibernate.Session;
 
@@ -16,23 +18,18 @@ public class App {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-Employee e =new Employee("Ankur","male",94009);
+Employee e =new Employee("Subham","male",94009);
 
-//SessionFactory sessionFactory=new Configuration().configure("hibernate.cfg.xml").buildSessionFactory();
-//StandardServiceRegistry ssr= new StandardServiceRegistryBuilder().configure("hibernate.cfg.xml").build();
-//Metadata meta=new MetadataSources(ssr).getMetadataBuilder().build();
-//SessionFactory sessionfactory =meta.buildSessionFactory();
-//Session session=sessionFactory.openSession();
+SessionFactory sessionFactory = HibernateConfiguration.getSessionFactory();
 
 
 
-SessionFactory sessionfactory =new MetadataSources(new StandardServiceRegistryBuilder().configure("hibernate.cfg.xml").build()).getMetadataBuilder().build().buildSessionFactory();
-Session session=sessionfactory.openSession();
+Session session=sessionFactory.openSession();
 	//session.save(e);//It can be used with auto inc(generrator) does not give error.
 
 	session.persist(e);//but it can not used with this.
 	session.beginTransaction().commit();
-	sessionfactory.close();
+	sessionFactory.close();
 	}
 
 }
--- src/main/java/com/hibernate/entity/Employee.java
+++ src/main/java/com/hibernate/entity/Employee.java
@@ -1,16 +1,18 @@
 package com.hibernate.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
-@Entity
+@Entity(name="emp_name")
 
 public class Employee {
 	@Id
 	@GeneratedValue(strategy =GenerationType.IDENTITY)
 private int id;
+	@Column(name="user_name")
 private String name;
 private String gender;
 private int salary;
