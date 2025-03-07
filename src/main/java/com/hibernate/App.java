74d62e495df27e865e6be556bd448843cb97d56d
Shivang Yadav <01yadavshivang@gmail.com>
2025-03-07 10:32:12 +0530

    update

--- src/main/java/com/hibernate/App.java
+++ src/main/java/com/hibernate/App.java
@@ -1,9 +1,13 @@
 package com.hibernate;
 
 import org.hibernate.cfg.Configuration;
+
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
-
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.boot.registry.StandardServiceRegistry;
 import com.hibernate.entity.Employee;
 import org.hibernate.Session;
 
@@ -14,12 +18,21 @@ public class App {
 		// TODO Auto-generated method stub
 Employee e =new Employee("Ankur","male",94009);
 
-Configuration cfg =new Configuration().configure("hibernate.cfg.xml");
-SessionFactory sessionFactory=cfg.buildSessionFactory();
-Session session=sessionFactory.openSession();	
-Transaction tx=session.beginTransaction();
-	session.save(e);
-	tx.commit();
+//SessionFactory sessionFactory=new Configuration().configure("hibernate.cfg.xml").buildSessionFactory();
+//StandardServiceRegistry ssr= new StandardServiceRegistryBuilder().configure("hibernate.cfg.xml").build();
+//Metadata meta=new MetadataSources(ssr).getMetadataBuilder().build();
+//SessionFactory sessionfactory =meta.buildSessionFactory();
+//Session session=sessionFactory.openSession();
+
+
+
+SessionFactory sessionfactory =new MetadataSources(new StandardServiceRegistryBuilder().configure("hibernate.cfg.xml").build()).getMetadataBuilder().build().buildSessionFactory();
+Session session=sessionfactory.openSession();
+	//session.save(e);//It can be used with auto inc(generrator) does not give error.
+
+	session.persist(e);//but it can not used with this.
+	session.beginTransaction().commit();
+	sessionfactory.close();
 	}
 
 }
