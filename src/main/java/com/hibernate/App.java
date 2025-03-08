d27308a52f3aa2f4495e59eb7e07f3e586751fe7
Shivang Yadav <01yadavshivang@gmail.com>
2025-03-08 14:20:24 +0530

    update

--- src/main/java/com/hibernate/App.java
+++ src/main/java/com/hibernate/App.java
@@ -15,7 +15,7 @@ public class App {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 Employee e1 =new Employee("Mukul","male",94009);
-Employee e2=new Employee("Nishant","male",94009);
+Employee e2=new Employee("Nishant","male",194009);
 Employee e3 =new Employee("Shivang","male",94009);
 Employee e4=new Employee("Raj","male",94009);
 Employee e5 =new Employee("Ram","male",94009);
@@ -26,13 +26,13 @@ SessionFactory sessionFactory = HibernateConfiguration.getSessionFactory();
 Session session=sessionFactory.openSession();
 	//session.save(e);//It can be used with auto inc.(generator) does not give error.
 //	session.persist(e1);//but it can not used with this.
-//	session.persist(e2);
+	//session.persist(e2);
 //	session.persist(e3);
 //	session.persist(e4);
 //	session.persist(e5);
 //	session.beginTransaction().commit();
 //sessionFactory.close();
-//
+// 
 //Query query=session.createQuery("from Employee",Employee.class);
 //List list= query.list();
 //System.out.println(list);
@@ -47,14 +47,22 @@ Session session=sessionFactory.openSession();
 
 Transaction tx=session.beginTransaction();
 Query query=session.createQuery("update Employee set name=:n where id=:i ");
-query.setParameter("n", "Aviral");
+query.setParameter("n", "Ankur");
 query.setParameter("i", 3);
 query.executeUpdate();
 tx.commit();
 
-Query query1=session.createQuery("from Employee",Employee.class);
-List list=query.list();
-System.out.println(list);
+//Query query=session.createQuery("delete from Employee where id =10 ");
+//query.executeUpdate();
+//tx.commit();
+//Query query1=session.createQuery("from Employee",Employee.class);
+//List list=query.list();
+//System.out.println(list);
+
+//Query query=session.createQuery("select max(salary) from Employee");
+//List list =query.list();
+//System.out.print(list);
+
 sessionFactory.close();
 
 	}
